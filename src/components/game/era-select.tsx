4c41d1129ad1b91553da969b0fbb4e3dfa90cc75
James E <4589250+Naught0@users.noreply.github.com>
2024-10-13 15:41:55 -0400

    Type button

--- src/components/game/era-select.tsx
+++ src/components/game/era-select.tsx
@@ -12,7 +12,11 @@ export function EraSelect({
     onChange(value === Era.BCE ? Era.CE : Era.BCE);
   }
   return (
-    <Button className="h-[46px] rounded-l-none" onClick={toggleEra}>
+    <Button
+      type="button"
+      className="h-[46px] rounded-l-none"
+      onClick={toggleEra}
+    >
       {value}
     </Button>
   );
