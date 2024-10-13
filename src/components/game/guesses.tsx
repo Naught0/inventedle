fab6968ca9b9d428175406e80c1dac02212b459a
James E <4589250+Naught0@users.noreply.github.com>
2024-10-13 18:04:37 -0400

    Update colors n ui

--- src/components/game/guesses.tsx
+++ src/components/game/guesses.tsx
@@ -10,16 +10,23 @@ import {
 } from "./logic";
 import { InventionSelect } from "@/db/schema";
 
+export function GuessPlaceholder() {
+  return (
+    <li className="flex items-center gap-2 rounded-md bg-secondary px-3 py-2 text-lg font-bold lg:text-xl">
+      <span className="invisible">Text</span>
+    </li>
+  );
+}
 export function Guess(props: { guess: number; guessDistance: number }) {
   const getBgClassName = () => {
     const color = getGuessDistanceColor(props.guessDistance);
     switch (color) {
       case "green":
-        return "bg-green-600";
+        return "bg-green-700";
       case "yellow":
         return "bg-yellow-600";
       case "red":
-        return "bg-red-600";
+        return "bg-destructive";
     }
   };
   const getIcon = () => {
@@ -35,17 +42,25 @@ export function Guess(props: { guess: number; guessDistance: number }) {
     <li
       className={`flex items-center gap-2 rounded-md px-3 py-2 text-lg font-bold lg:text-xl ${getBgClassName()}`}
     >
-      {getIcon()}
+      <span className="text-2xl">{getIcon()}</span>
       {props.guess}
     </li>
   );
 }
-export function Guesses(props: {
+export function Guesses({
+  totalAllowedGuesses,
+  ...props
+}: {
   invention: InventionSelect;
   guesses: number[];
+  totalAllowedGuesses: number;
 }) {
   return (
     <ul className="flex flex-col gap-2">
+      {props.guesses.length < totalAllowedGuesses &&
+        [...Array(totalAllowedGuesses - props.guesses.length).keys()].map(
+          (idx) => <GuessPlaceholder key={idx} />,
+        )}
       {props.guesses.map((guess) => (
         <Guess
           key={`${guess}`}
