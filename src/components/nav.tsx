d76b6b08567159001d3f2a8e19f9ac1eb7305751
James E <4589250+Naught0@users.noreply.github.com>
2024-11-03 22:58:05 -0500

    Update header style

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -2,12 +2,14 @@ import React from "react";
 
 export function Nav() {
   return (
-    <nav className="flex flex-row justify-center gap-3 px-12 py-6 text-xl">
-      <div className="inline-flex w-full max-w-screen-md items-center justify-center gap-3">
+    <nav className="flex flex-row justify-center gap-3 px-12 py-6 text-center">
+      <div className="inline-flex w-full max-w-screen-md flex-wrap items-center justify-center gap-3">
         <strong className="border-b border-r border-b-white border-r-white p-3 text-3xl lg:text-4xl">
-          <span className="text-muted-foreground">Invented</span>le
+          <span className="text-primary">Invented</span>le
         </strong>
-        <span>the inventurous daily guessing game</span>
+        <span className="text-base italic lg:text-xl">
+          the inventurous daily guessing game
+        </span>
       </div>
     </nav>
   );
