10834f6953714e22e8cfdeaed8611bdc630bef6d
James E <4589250+Naught0@users.noreply.github.com>
2024-11-03 22:51:43 -0500

    Satisfy eslint & add more rules

--- src/scrape/wiki.ts
+++ src/scrape/wiki.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars  */
 import type { Invention, Prisma } from "@prisma/client";
 import { writeFile } from "fs/promises";
 import { JSDOM } from "jsdom";
