980e6132889274b5cdf91c81ad9ee739888e5f95
James E <4589250+Naught0@users.noreply.github.com>
2025-12-08 02:58:53 -0500

    Dockerize

--- src/db/prisma/index.ts
+++ src/db/prisma/index.ts
@@ -2,21 +2,18 @@ import "dotenv/config";
 import { PrismaClient } from "../prisma/generated/client";
 import { PrismaLibSql } from "@prisma/adapter-libsql";
 
-// let client: PrismaClient;
-// if (process.env.NODE_ENV === "production") {
-//   client = new PrismaClient({
-//     adapter: new PrismaLibSql({
-//       url: `${process.env.TURSO_DATABASE_URL}`,
-//       authToken: `${process.env.TURSO_AUTH_TOKEN}`,
-//     }),
-//   });
-// } else if (process.env.NODE_ENV === "development") {
-//   client = new PrismaClient({
-//     adapter: new PrismaLibSql({ url: `${process.env.DATABASE_URL}` }),
-//   });
-// }
-const client = new PrismaClient({
-  adapter: new PrismaLibSql({ url: `${process.env.DATABASE_URL}` }),
-});
+let client: PrismaClient;
+if (process.env.NODE_ENV === "production") {
+  client = new PrismaClient({
+    adapter: new PrismaLibSql({
+      url: `${process.env.TURSO_DATABASE_URL}`,
+      authToken: `${process.env.TURSO_AUTH_TOKEN}`,
+    }),
+  });
+} else if (process.env.NODE_ENV === "development") {
+  client = new PrismaClient({
+    adapter: new PrismaLibSql({ url: `${process.env.DATABASE_URL}` }),
+  });
+}
 
 export { client };
