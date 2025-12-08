e8b28635ecc511869efde2f9bc4ecfdd9988e4ce
James E <4589250+Naught0@users.noreply.github.com>
2025-12-08 01:00:35 -0500

    Bupkis

--- src/scrape/ai.ts
+++ src/scrape/ai.ts
@@ -3,7 +3,7 @@ import { readFileSync, writeFileSync } from "fs";
 import ollama from "ollama";
 import pLimit from "p-limit";
 
-const limit = pLimit(3);
+const limit = pLimit(5);
 
 async function populateNameFromDescription() {
   const inventions = JSON.parse(readFileSync("inventions.json").toString()) as {
@@ -11,45 +11,53 @@ async function populateNameFromDescription() {
     start_year: number;
     end_year?: number;
   }[];
-  const out = await Promise.all(
-    inventions.map(async (i) => {
-      console.log(i.description);
-      return limit(async () => ({
+  let processed = 0;
+  const promises = inventions.map(async (i) => {
+    return limit(async () => {
+      const resp = {
         ...i,
         name: (
           await ollama.chat({
-            model: "gemma3:4b",
+            model: "gemma3:12b-it-qat",
             messages: [
               {
                 role: "system",
                 content:
-                  "Reply in as few words as possible while being accurate. Do not include years or dates in your reply. Do not include any information about time in your reply.",
+                  "Reply in as few words as possible while being accurate. Do not include years or dates in your reply. Do not include any information about time in your reply. Do not be overly generic. Do not extrapolate.",
               },
               {
                 role: "user",
-                content: `What is the invention being mentioned in the following sentence? ${i.description}. Your answer should be succinct as it will be used in a guessing game in which a user must guess the words or concepts described.`,
+                content: `What is the invention being mentioned in the following sentence? Your answer should be succinct as it will be used in a guessing game in which a user must guess the words or concepts described. You must use the most common name for whatever invention/concept is described. The sentence is:\n"${i.description}."`,
               },
             ],
           })
         ).message.content,
-      }));
-    }),
-  );
+      };
+
+      processed += 1;
+      console.log("Processed", processed, "/", promises.length);
+      return resp;
+    });
+  });
+  const out = await Promise.all(promises);
 
   writeFileSync("inventions_ai.json", JSON.stringify(out, undefined, 2));
 }
 
 async function writeToDb() {
   const inventions = JSON.parse(
-    readFileSync("inventions_ai.json").toString(),
+    readFileSync("inventions.britannica.json").toString(),
   ) as {
-    description: string;
-    start_year: number;
-    end_year?: number;
+    image_url: string;
+    inventor: string;
+    inventor_link: string;
     name: string;
+    invention_link: string;
+    year: string;
+    description: string;
   }[];
   for (const data of inventions) {
-    await db.invention.create({ data });
+    await db.invention.create({ data: { ...data, year: parseInt(data.year) } });
   }
 }
 
