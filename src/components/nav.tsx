506de5421706bfc3a03008b6dcc653dd8e5aa35e
James E <4589250+Naught0@users.noreply.github.com>
2025-12-09 23:35:56 -0500

    Padding

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -3,7 +3,7 @@ import Logo from "./logo";
 
 export function Nav() {
   return (
-    <nav className="flex flex-row flex-wrap items-center justify-center gap-3 px-10 py-3 text-center md:px-12 md:py-6">
+    <nav className="flex flex-row flex-wrap items-center justify-center gap-3 px-10 py-3 text-center md:px-12">
       <div className="flex w-full max-w-screen-md flex-shrink basis-[512px] flex-col flex-wrap items-center justify-center gap-3 font-mono">
         <Logo />
         <span className="text-base italic lg:text-xl">
