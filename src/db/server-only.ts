5e25d305652dcf764770e4c8eca7f38c75324401
James E <4589250+Naught0@users.noreply.github.com>
2025-12-12 01:38:54 -0500

    Avoid bad inventions

--- src/db/server-only.ts
+++ src/db/server-only.ts
@@ -4,8 +4,11 @@ import { isSameDay, subMonths } from "date-fns";
 import { db } from ".";
 import { getIOTD, getRandomInvention } from "./actions";
 
+/**
+ * Creates an Invention of the Day unless one has already been created for today (EST)
+ *
+ */
 export async function createIOTD() {
-  /// Creates an Invention of the Day unless one has already been created for today (EST)
   const now = new TZDate(new Date(), "America/New_York");
   const iotd = await getIOTD();
 
@@ -14,18 +17,39 @@ export async function createIOTD() {
   }
 
   console.log(`Creating IOTD for ${now.toDateString()} at`, now);
-  const avoidInventions = await db.inventionOfTheDay.findMany({
-    select: { invention_id: true },
-    distinct: ["invention_id"],
-    where: {
-      created_at: {
-        gt: subMonths(now, 2),
+  const avoidIOTDs = (
+    await db.inventionOfTheDay.findMany({
+      select: { invention_id: true },
+      distinct: ["invention_id"],
+      where: {
+        created_at: {
+          gt: subMonths(now, 3),
+        },
       },
-    },
-  });
-  const invention = await getRandomInvention(
-    avoidInventions.map((i) => i.invention_id),
+    })
+  ).map((i) => i.invention_id);
+  const avoidBadInventions = (
+    await db.invention.findMany({
+      select: { id: true },
+      distinct: ["id"],
+      where: {
+        OR: [
+          { id: { in: avoidIOTDs } },
+          { image_url: { equals: null } },
+          { description: { equals: "" } },
+          { invention_link: { equals: null } },
+        ],
+      },
+    })
+  ).map((i) => i.id);
+  console.log(
+    `Avoiding ${avoidIOTDs.length} IOTDs and ${avoidBadInventions.length} bad inventions`,
   );
+
+  const invention = await getRandomInvention([
+    ...avoidIOTDs,
+    ...avoidBadInventions,
+  ]);
   const newIotd = await db.inventionOfTheDay.create({
     data: {
       invention_id: invention.id,
