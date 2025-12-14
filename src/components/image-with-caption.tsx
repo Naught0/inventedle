49b3bdbd64b1d75e456f39ecbea233c7e7b5577a
James E <4589250+Naught0@users.noreply.github.com>
2025-12-13 22:58:28 -0500

    Fix props

--- src/components/image-with-caption.tsx
+++ src/components/image-with-caption.tsx
@@ -1,13 +1,12 @@
 import { cn } from "@/lib/utils";
-import { ImageProps } from "next/image";
-import { ReactNode } from "react";
+import { HTMLProps, ReactNode } from "react";
 
 export function ImageWithCaption({
   children,
   alt,
   className,
   ...props
-}: ImageProps & { children?: ReactNode }) {
+}: HTMLProps<HTMLImageElement> & { children?: ReactNode }) {
   return (
     <div className="flex w-full flex-col items-center justify-center gap-2">
       <img className={cn("rounded-lg", className)} alt={alt} {...props} />
