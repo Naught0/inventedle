91dd377cc97dbae176d8a925cade848f846e29a9
James E <4589250+Naught0@users.noreply.github.com>
2025-12-15 15:39:51 -0500

    Better 404

--- src/app/stats/[userId]/not-found.tsx
+++ src/app/stats/[userId]/not-found.tsx
@@ -5,17 +5,16 @@ import { PiHouseFill } from "react-icons/pi";
 export default function NotFound() {
   return (
     <div className="flex w-full flex-col items-center justify-center gap-12">
-      <h2 className="font-regular text-muted-foreground inline-flex flex-col flex-wrap items-center justify-center gap-6 text-center text-4xl font-bold sm:flex-row">
-        <span className="text-primary text-8xl font-light">404</span>
-        <span className="bg-muted-foreground hidden h-32 w-1 sm:block"></span>{" "}
-        <span className="">User not found</span>
+      <h2 className="font-regular text-muted-foreground flex flex-col flex-wrap items-center justify-center gap-6 text-center text-4xl font-bold">
+        <span className="text-primary text-8xl font-extrabold">404</span>
+        <span className="font-extralight">User not found</span>
       </h2>
       <Link
         prefetch={false}
         href="/"
         className={buttonVariants({ size: "xl", className: "gap-2" })}
       >
-        <PiHouseFill /> Home
+        <PiHouseFill /> Go Home
       </Link>
     </div>
   );
