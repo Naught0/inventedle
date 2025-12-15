2b74ee00f25ff2614eb601fbdb3ae9992422b783
James E <4589250+Naught0@users.noreply.github.com>
2025-12-15 11:46:53 -0500

    Generate user open graph metadata

--- src/app/stats/[userId]/page.tsx
+++ src/app/stats/[userId]/page.tsx
@@ -1,7 +1,49 @@
 import { UserStats } from "@/components/user-stats";
 import { db } from "@/db";
 import { getUserGameStats } from "@/db/server-only";
+import { getServerSession } from "@/lib/auth";
+import { Metadata } from "next";
 import { notFound } from "next/navigation";
+
+export const dynamic = "force-dynamic";
+
+export const generateMetadata = async ({
+  params,
+}: {
+  params: Promise<{ userId: string }>;
+}): Promise<Metadata> => {
+  const { userId } = await params;
+  const user = await db.user.findUnique({ where: { id: userId } });
+  if (!user?.isPublic)
+    return {
+      title: "Inventedle",
+      description: "User not found",
+      openGraph: {
+        url: "/",
+      },
+    };
+
+  const totalWins = await db.result.count({
+    where: {
+      user_id: userId,
+      win: true,
+    },
+  });
+
+  const images = [{ url: user.image ?? "" }];
+
+  return {
+    title: "Inventedle",
+    description: `${user.name}'s Stats - ${totalWins} win${totalWins === 1 ? "" : "s"}`,
+    twitter: {
+      card: "summary",
+    },
+    openGraph: {
+      url: `https://inventedle.jamese.dev/stats/${user.id}`,
+      images,
+    },
+  };
+};
 
 export default async function Page({
   params,
@@ -10,7 +52,10 @@ export default async function Page({
 }) {
   const { userId } = await params;
   const user = await db.user.findUnique({ where: { id: userId } });
-  if (!user?.isPublic) notFound();
+  if (!user) notFound();
+
+  const session = await getServerSession();
+  if (!user?.isPublic && !(session?.user.id !== user.id)) notFound();
 
   const stats = await getUserGameStats(userId);
   if (!stats) notFound();
--- src/app/stats/page.tsx
+++ src/app/stats/page.tsx
@@ -1,17 +1,9 @@
-import { UserStats } from "@/components/user-stats";
-import { getUserGameStats } from "@/db/server-only";
 import { getServerSession } from "@/lib/auth";
-import { notFound } from "next/navigation";
-
-export const dynamic = "force-dynamic";
+import { notFound, redirect, RedirectType } from "next/navigation";
 
 export default async function Page() {
   const session = await getServerSession();
-  const user = session?.user;
-  const stats = session?.user.id
-    ? await getUserGameStats(session.user.id)
-    : undefined;
-  if (!stats) notFound();
+  if (!session?.user) notFound();
 
-  return <UserStats user={user} stats={stats} />;
+  redirect(`/stats/${session.user.id}`, RedirectType.replace);
 }
