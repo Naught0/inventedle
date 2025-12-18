efc7e294355ae9fe930d0fa3b118f3bc857d388a
James E <4589250+Naught0@users.noreply.github.com>
2025-12-17 22:35:04 -0500

    Break out section heading

---
+++ src/app/profile/section-heading.tsx
@@ -0,0 +1,17 @@
+import { Separator } from "@radix-ui/react-separator";
+import { cn } from "better-auth/dist/index-BZSqJoCN.mjs";
+
+export function SectionHeading({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) {
+  return (
+    <div className="my-3 flex flex-grow flex-col items-start gap-3">
+      <h3 className={cn("text-2xl font-extralight", className)}>{children}</h3>
+      <Separator className="bg-status-error-foreground/40" />
+    </div>
+  );
+}
