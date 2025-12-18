a69933fd1496b5d8fcf5bcd11449c9fa34698999
James E <4589250+Naught0@users.noreply.github.com>
2025-12-18 00:45:35 -0500

    Add loading overlay

---
+++ src/components/ui/loading-overlay.tsx
@@ -0,0 +1,9 @@
+import { CgSpinner } from "react-icons/cg";
+
+export function LoadingOverlay() {
+  return (
+    <div className="absolute bottom-0 left-0 right-0 top-0 flex h-full w-full items-center justify-center rounded-lg bg-black/30">
+      <CgSpinner className="animate-spin text-4xl" />
+    </div>
+  );
+}
