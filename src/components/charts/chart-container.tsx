d552da5949211d7b8f50b82a3085424030ed9d4e
James E <4589250+Naught0@users.noreply.github.com>
2025-12-19 03:47:27 -0500

    Make chart containerUpdate chart container styles

--- src/components/charts/chart-container.tsx
+++ src/components/charts/chart-container.tsx
@@ -5,7 +5,7 @@ export function ChartContainer({
   title,
 }: PropsWithChildren<{ title: string }>) {
   return (
-    <div className="bg-accent flex min-h-72 w-[512px] max-w-full flex-grow flex-col items-center justify-start rounded-lg p-3 lg:p-5">
+    <div className="bg-accent flex min-h-96 w-[512px] max-w-full flex-col items-center justify-start rounded-lg p-4 lg:p-6">
       <h4 className="h-fit text-center text-xl font-extralight uppercase">
         {title}
       </h4>
