f748f1c4a203b7fefe641b3918c7e41f041e6bab
James E <4589250+Naught0@users.noreply.github.com>
2025-12-20 01:20:47 -0500

    Center text if it wraps

--- src/components/ui/stat-card.tsx
+++ src/components/ui/stat-card.tsx
@@ -8,11 +8,11 @@ export const StatCard = ({
   children: ReactNode;
 }) => {
   return (
-    <figure className="bg-accent flex min-h-24 min-w-36 flex-grow flex-col items-center justify-center gap-3 rounded-lg p-3 md:min-h-32 md:min-w-48 lg:p-5">
+    <figure className="bg-accent flex min-h-32 min-w-36 flex-grow flex-col items-center justify-center gap-3 rounded-lg p-3 text-center md:min-w-48 lg:p-5">
       <figcaption className="text-muted-foreground font-lighter uppercase">
         {title}
       </figcaption>
-      <span className="text-foreground text-4xl font-extrabold">
+      <span className="text-foreground text-3xl font-extrabold lg:text-4xl">
         {children}
       </span>
     </figure>
