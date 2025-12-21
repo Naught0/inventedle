f41233d7133628cda65797a9504bd34461ede4dd
James E <4589250+Naught0@users.noreply.github.com>
2025-12-21 02:57:22 -0500

    Add center prop to stack

--- src/components/ui/stack.tsx
+++ src/components/ui/stack.tsx
@@ -4,15 +4,17 @@ export function Stack({
   children,
   className,
   horizontal = false,
+  center = false,
 }: {
   children: React.ReactNode;
   className?: string;
   horizontal?: boolean;
+  center?: boolean;
 }) {
   return (
     <div
       className={cn(
-        `flex gap-3 ${horizontal ? "flex-row" : "flex-col"}`,
+        `flex gap-3 ${center ? "items-center justify-center" : ""} ${horizontal ? "flex-row" : "flex-col"}`,
         className,
       )}
     >
