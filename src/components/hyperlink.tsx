4cd0f0c626435a56ae4d6dd9004281c80025e91d
James E <4589250+Naught0@users.noreply.github.com>
2026-01-13 22:32:41 -0500

    Use next/link

--- src/components/hyperlink.tsx
+++ src/components/hyperlink.tsx
@@ -1,4 +1,5 @@
 import { cn } from "@/lib/utils";
+import Link, { LinkProps } from "next/link";
 import { HTMLProps, PropsWithChildren } from "react";
 import { PiArrowUpRight } from "react-icons/pi";
 
@@ -8,9 +9,9 @@ export function Hyperlink({
   rel,
   target,
   ...props
-}: PropsWithChildren<HTMLProps<HTMLAnchorElement>>) {
+}: PropsWithChildren<HTMLProps<HTMLAnchorElement>> & LinkProps) {
   return (
-    <a
+    <Link
       className={cn(
         "hover:text-primary text-muted-foreground inline-flex items-center truncate text-wrap break-all underline-offset-4 transition-colors hover:underline",
         className,
@@ -23,6 +24,6 @@ export function Hyperlink({
         {children}
         <PiArrowUpRight className="inline" />
       </span>
-    </a>
+    </Link>
   );
 }
