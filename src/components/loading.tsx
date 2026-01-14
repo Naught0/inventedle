9e514b447dcb25037c6cd15104c58aa2c072e7c4
James E <4589250+Naught0@users.noreply.github.com>
2026-01-13 23:56:14 -0500

    Improve loading page responsiveness

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -1,18 +1,19 @@
 import { Skeleton } from "@/components/ui/skeleton";
+import { Stack } from "./ui/stack";
 
 export function LoadingGame() {
   return (
-    <div className="flex w-full max-w-md flex-col items-center justify-center gap-6 lg:max-w-screen-lg lg:gap-6">
-      <div className="flex flex-col items-center gap-3">
+    <Stack className="w-full gap-6 sm:max-w-md lg:max-w-screen-lg" center>
+      <Stack center>
         <h2 className="text-3xl font-extrabold">Loading game...</h2>
         <Skeleton className="h-8 w-48" />
         <Skeleton className="h-6 w-52" />
-      </div>
-      <Skeleton className="h-16 w-[512px]" />
+      </Stack>
+      <Skeleton className="h-16 w-full max-w-[512px]" />
       <div className="grid w-full grid-cols-1 gap-3 lg:grid-cols-2 lg:gap-6">
         <div className="flex w-full flex-col justify-start">
           <div className="flex flex-1 basis-1/2 flex-col justify-start gap-3 rounded-lg lg:gap-6">
-            <Skeleton className="h-96 w-full max-w-[512px] rounded-lg" />
+            <Skeleton className="m-auto h-96 w-full max-w-[512px] rounded-lg" />
           </div>
         </div>
         <div className="flex flex-col gap-3">
@@ -20,7 +21,7 @@ export function LoadingGame() {
           <Skeleton className="h-12 w-36" />
         </div>
       </div>
-      <Skeleton className="h-96 w-[512px]" />
-    </div>
+      <Skeleton className="h-96 w-full max-w-[512px]" />
+    </Stack>
   );
 }
