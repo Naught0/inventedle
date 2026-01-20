d05f8ba06def5114131d0899063c28516f6faa6a
James E <4589250+Naught0@users.noreply.github.com>
2026-01-20 01:21:30 -0500

    Fix charts not appearing in firefox by adding height

--- src/components/charts/chart-container.tsx
+++ src/components/charts/chart-container.tsx
@@ -5,7 +5,7 @@ export function ChartContainer({
   title,
 }: PropsWithChildren<{ title: string }>) {
   return (
-    <div className="bg-accent flex min-h-96 w-full max-w-[512px] flex-col items-center justify-start rounded-lg p-4 font-mono lg:p-6">
+    <div className="bg-accent flex h-full min-h-96 w-full max-w-[512px] flex-col items-center justify-start rounded-lg p-4 font-mono lg:p-6">
       <h4 className="text-muted-foreground h-fit text-center text-xl font-extralight uppercase">
         {title}
       </h4>
