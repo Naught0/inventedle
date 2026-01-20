de6e9ab7caf60789dafe73059f08c2135a9c4b3d
James E <4589250+Naught0@users.noreply.github.com>
2026-01-20 00:30:40 -0500

    Add hooks to handle backfill logic

---
+++ src/components/hooks/use-backfill-results.ts
@@ -0,0 +1,82 @@
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { LocalGame, localStorageKeyBase } from "./use-game-recorder";
+import { useMemo } from "react";
+
+export function useMissingIotds({ userId }: { userId?: string }) {
+  return useQuery({
+    queryKey: ["iotdsMissingResults", userId],
+    queryFn: async () => {
+      const resp = (await (
+        await fetch(`/api/user/iotds-without-results`, {
+          method: "GET",
+        })
+      ).json()) as { id: number }[];
+      return resp;
+    },
+    select: (data) => {
+      return data.map((r) => r.id);
+    },
+    enabled: !!userId,
+  });
+}
+
+export function useMissingResultsToImport({ userId }: { userId?: string }) {
+  const { data: missingIotds, refetch: refetchMissingIotds } = useMissingIotds({
+    userId,
+  });
+
+  const localResultsToImport = useMemo(() => {
+    if (!missingIotds || !missingIotds.length) return [];
+
+    const localResults: LocalGame[] = [];
+    for (const id of missingIotds) {
+      const localResult = localStorage.getItem(`${localStorageKeyBase}-${id}`);
+      if (localResult) {
+        localResults.push(JSON.parse(localResult) as LocalGame);
+      }
+    }
+    return localResults;
+  }, [missingIotds]);
+
+  return {
+    data: localResultsToImport,
+    refetch: refetchMissingIotds,
+  };
+}
+
+export function useBackfillResults({ userId }: { userId?: string }) {
+  const { data: localGamesToImport, refetch } = useMissingResultsToImport({
+    userId,
+  });
+  const { mutateAsync, isPending, error, data, isSuccess, isError } =
+    useMutation({
+      mutationKey: ["backfill-results", userId],
+      mutationFn: async (result: LocalGame[]) => {
+        const resp = await fetch("/api/game/record-results", {
+          method: "POST",
+          body: JSON.stringify(result),
+        });
+        if (!resp.ok) {
+          return Promise.reject(await resp.json());
+        } else {
+          return (await resp.json()) as { data: number[] };
+        }
+      },
+      onSuccess() {
+        refetch();
+      },
+    });
+  const backfill = async () => {
+    return await mutateAsync(localGamesToImport);
+  };
+
+  return {
+    backfill,
+    localGamesToImport,
+    data: data?.data,
+    error,
+    isPending,
+    isSuccess,
+    isError,
+  };
+}
