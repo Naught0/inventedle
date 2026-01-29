a0ed89d39728781d8f978f167fda0281069535a0
James E <4589250+Naught0@users.noreply.github.com>
2026-01-29 00:27:41 -0500

    Handle and log errors

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -4,6 +4,7 @@ import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 import { redirect, RedirectType } from "next/navigation";
 import { generateIOTDMeta } from "@/actions/server-only";
+import { InventionOfTheDayGetPayload } from "@/db/prisma/generated/models";
 
 export const dynamic = "force-dynamic";
 
@@ -20,11 +21,25 @@ export default async function Page({
   params: Promise<{ id: string }>;
 }) {
   const { id } = await params;
-  const iotd = await db.inventionOfTheDay.findUnique({
-    where: { id: parseInt(id) },
-    include: { invention: true },
-  });
+  if (!id) {
+    console.error("Go no ID on app/[id]/page");
+    return redirect(`/`, RedirectType.replace);
+  }
+  let iotd: InventionOfTheDayGetPayload<{
+    include: { invention: true };
+  }> | null;
+  try {
+    iotd = await db.inventionOfTheDay.findUnique({
+      where: { id: parseInt(id) },
+      include: { invention: true },
+    });
+  } catch {
+    console.error("Prisma Error: No IOTD found for ID", id);
+    return redirect(`/`, RedirectType.replace);
+  }
+
   if (!iotd) {
+    console.error("No IOTD found for ID", id);
     return redirect(`/`, RedirectType.replace);
   }
 
